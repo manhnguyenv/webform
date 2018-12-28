49b6d3215651436bf2b5b75f7e540cca20c9f7ff
Nguyen Viet Manh <manhng@outlook.com>
2018-12-29 01:38:46 +0700

    Correct

---
+++ DTO/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace DTO
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return string.Concat("{", $"'Id': {this.Id}', 'Name': '{this.Name}.'", "}");
+        }
+    }
+}
---
+++ Entity/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Entity
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
